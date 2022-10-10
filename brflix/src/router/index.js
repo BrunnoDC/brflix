eb974aeaafcc4e31e5a463e281388e7c83c831cf
paulozemr <pauloramosj213@gmail.com>
2022-10-10 15:11:35 -0300

    calica

--- brflix/src/router/index.js
+++ brflix/src/router/index.js
@@ -1,22 +1,27 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import catalagoVag from '../views/catalagoVag.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import FilmesView from "../views/FilmesView.vue";
+import catalagoVag from "../views/catalagoVag.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-      path: '/list',
-      name: 'list',
-      component: catalagoVag
+      path: "/list",
+      name: "list",
+      component: catalagoVag,
     },
+    {
+      path: "/filmes",
+      name: "filmes",
+      component: FilmesView,
+    },
+  ],
+});
 
-  ]
-})
-
-export default router
+export default router;
--- brflix/src/views/FilmesView.vue
+++ brflix/src/views/FilmesView.vue
@@ -56,19 +56,7 @@ export default {
   },
 };
 </script>
-
 <template>
-  <div class="main">
-    <div class="lista">
-      <h2 class="txtprin">Mais Procurados</h2>
-      <hr />
-      <div class="categorias">
-        <span>ação</span>
-        <span>Comedia</span>
-        <span>Drama</span>
-      </div>
-    </div>
-  </div>
   <div class="">
     <div v-for="genero of generos" :key="genero.id">
       <h1 class="flex justify-center text-white text-4xl pb-7">
@@ -86,30 +74,3 @@ export default {
     </div>
   </div>
 </template>
-<style>
-.bemvindo {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-top: 35%;
-  color: white;
-  font-size: 1.8rem;
-  font-family: Arial, Helvetica, sans-serif;
-  font-weight: bold;
-  background-color: black;
-}
-
-.bemvindo button {
-  padding: 15px;
-  border-radius: 25px;
-  text-decoration: none;
-  border: none;
-  min-width: 140px;
-  cursor: pointer;
-  background-color: #1db954;
-  margin-top: 15px;
-}
-.btnenter a {
-  color: black;
-}
-</style>
